54643a67d4827c1242e9aa965f4d660e6187c9fd
Vinay <Vaddanki@github.com>
2017-10-13 16:17:32 +0530

    Added puzzle package and code

--- src/puzzle/gajanan/BuyingShowTickets.java
+++ src/puzzle/gajanan/BuyingShowTickets.java
@@ -1,14 +1,33 @@
 package puzzle.gajanan;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class BuyingShowTickets {
 
-	public static void main(String[] args) {
-	
+	@SuppressWarnings("resource")
+	public static void main(String[] args) throws IOException {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Input array size:");
+		int n = sc.nextInt();
+		int tickets[] = new int[n];
+		System.out.println(tickets.length + " Input array elements:");
+		for (int i = 0; i < n; i++) {
+			tickets[i] = sc.nextInt();
+		}
+		System.out.println("Position in array:");
+		int p = sc.nextInt();
+		int r = waitingTime(tickets, p);
+		System.out.println("Output: " + r);
+
 	}
 
 	static int waitingTime(int[] tickets, int p) {
 
-		return p;
+		int outputTime = 0;
+
+		return outputTime;
 	}
 
 }
