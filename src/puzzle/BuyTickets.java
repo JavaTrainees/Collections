58f9a35dd04accf6c1c70033b5831396a89789d4
JavaTrainees <32760726+JavaTrainees@users.noreply.github.com>
2017-10-13 16:59:30 +0530

    done

--- src/puzzle/BuyTickets.java
+++ src/puzzle/BuyTickets.java
@@ -1,28 +1,27 @@
 package puzzle;
-
-public class BuyTickets {
+class BuyTickets {
 
 	public static void main(String args[]) {
 
-		int index = 0, output = 0, time = 0, a[] = new int[] { 1,1,1,1 };
+		int index = 3,  time = 0, a[] = new int[] { 4,2,5,3 };
 
-		while (a[index] != 0) {
+		while (a[index] != 0){
 			if (a[0] > 0) {
 				a[0] = a[0] - 1;
 				time++;
 			}
-			output = updateArray(a);
+			//if(a[0] != 0)
+			//updateArray(a);
+			int n, temp = a[0];
+			n = a.length;
+			for (int i = 0; i < n - 1; i++)
+				a[i] = a[i + 1];
+			a[n - 1] = temp;
+			index = (index + n - 1) % n;
 		}
-		System.out.println(output);
+		System.out.println(time);
 	}
 
-	static int updateArray(int a[]) {
-		int n, index = 0, temp = a[0];
-		n = a.length;
-		for (int i = 0; i < n - 1; i++)
-			a[i] = a[i + 1];
-		a[n - 1] = temp;
-		return index = (index + n - 1) % n;
-	}
+	
 
 }
